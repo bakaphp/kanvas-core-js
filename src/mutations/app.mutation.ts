1a51974da307d16af388be3f6c6aba0fb609c93b
ghaerdi <ghaerdi@outlook.com>
2023-04-03 15:09:56 -0400

    format

--- src/mutations/app.mutation.ts
+++ src/mutations/app.mutation.ts
@@ -1,10 +1,7 @@
 import { gql } from '@apollo/client/core';
 
 export const USER_UPDATE_PASSWORD_MUTATION = gql`
-	mutation appUserUpdatePassword($uuid: String!, $password: String!){
-		appUserUpdatePassword(
-			uuid: $uuid
-			password: $password
-		)
-	}
+  mutation appUserUpdatePassword($uuid: String!, $password: String!) {
+    appUserUpdatePassword(uuid: $uuid, password: $password)
+  }
 `;
