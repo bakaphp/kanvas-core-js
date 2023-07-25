775f833f58e15dadc049428c926392977ee0484d
RivierGrullon <grullonrivier@gmail.com>
2023-07-25 11:30:23 -0400

    feat: Add getAllLeads query

--- src/modules/leads/index.ts
+++ src/modules/leads/index.ts
@@ -1,12 +1,7 @@
+import { GET_ALL_LEADS_QUERY } from '../../queries';
 import { ClientType } from '../../index';
-import {
-    CREATE_LEAD_MUTATION
-} from '../../mutations';
-import {
-  CreateLeadData,
-  CreateLeadParams,
-  LeadInput,
-} from '../../types';
+import { CREATE_LEAD_MUTATION } from '../../mutations';
+import { CreateLeadData, CreateLeadParams, LeadInput } from '../../types';
 
 export class Leads {
   constructor(protected client: ClientType) {}
@@ -14,14 +9,19 @@ export class Leads {
   public async createLead(
     leadData: CreateLeadParams | LeadInput
   ): Promise<CreateLeadData> {
-
-  
     const response = await this.client.mutate({
       mutation: CREATE_LEAD_MUTATION,
       variables: { input: leadData },
     });
 
     return response.data as CreateLeadData;
- }
+  }
 
+  public async getAllLeads(): Promise<CreateLeadData> {
+    const response = await this.client.query({
+      query: GET_ALL_LEADS_QUERY,
+    });
+
+    return response.data;
+  }
 }
