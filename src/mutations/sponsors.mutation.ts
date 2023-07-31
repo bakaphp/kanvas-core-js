135f08b8e1288f4869499a11ea994a6dae771c6a
Lizandro <lizandro@mctekk.com>
2023-07-31 18:00:22 -0400

    chore: mutations-types

--- src/mutations/sponsors.mutation.ts
+++ src/mutations/sponsors.mutation.ts
@@ -1,20 +1,8 @@
 import { gql } from '@apollo/client/core';
 
 export const INVITE_SPONSOR_MUTATION = gql`
- mutation(
-    $email: String!
-    $role_id: Int!
-    $companies_branches_id: Int!
-    $firstname: String
-    $lastname : String
-){
-    inviteUser(input: {
-        role_id: $role_id,
-        email: $email,
-        companies_branches_id:$companies_branches_id,
-        firstname : $firstname,
-        lastname: $lastname
-    }){
+ mutation( $input: InviteInput!){
+    inviteUser(input: $input){
         id
         email,
         invite_hash
--- src/types/sponsors.ts
+++ src/types/sponsors.ts
@@ -3,12 +3,21 @@
 // import { Location } from 'types/location.interface';
 // import { RoleInterface } from './role.interface';
 
+
+interface CustomFieldInput {
+    name: string;
+    data: any;
+}
+
 export interface InviteSponsorParams {
     email: string;
     firstname: string;
     lastname: string;
     role_id: number;
     company_branches_id: number;
+    //description: string;
+    //email_template: string;
+    custom_fields: CustomFieldInput[]
 }
 
 export interface InviteSponsorData {
