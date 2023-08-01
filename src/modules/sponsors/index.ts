d44a5bbec69813f584f93665abddb16aefa59330
Lizandro <lizandro@mctekk.com>
2023-08-01 09:39:26 -0400

    feat: add invite sponsor function

--- src/modules/sponsors/index.ts
+++ src/modules/sponsors/index.ts
@@ -5,12 +5,12 @@ import { InviteSponsorData, InviteSponsorParams } from '../../types';
 export class Sponsors {
     constructor(protected client: ClientType) {}
 
-    public async createLead(
-        leadData: InviteSponsorParams 
+    public async inviteSponsor(
+        sponsorInput: InviteSponsorParams 
     ): Promise<InviteSponsorData> {
         const response = await this.client.mutate({
             mutation: INVITE_SPONSOR_MUTATION,
-            variables: { input: leadData },
+            variables: { input: sponsorInput },
         });
 
         return response.data 
