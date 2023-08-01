ef843edb283b1a48f7fd8b454341d3782ef1ee95
RivierGrullon <grullonrivier@gmail.com>
2023-08-01 12:53:40 -0400

    chore(test): Update test description

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,6 +1,8 @@
-import { sum } from '../src';
+function sum(x, y) {
+  return x + y;
+}
 
-describe('blah', () => {
+describe('Test the CI', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
   });
