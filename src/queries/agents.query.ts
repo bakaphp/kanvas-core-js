7c4a58ec63c15d551626e61a2e183897ab3425af
Lizandro Ramirez <117939390+lizandro-mc@users.noreply.github.com>
2023-09-11 20:09:47 -0400

    chore: add TODO comments for agents queries

--- src/queries/agents.query.ts
+++ src/queries/agents.query.ts
@@ -1,5 +1,13 @@
 import { gql } from '@apollo/client/core';
 
+/*
+ TODO: Missing paginatorInfo 
+ TODO: Missing number of leads created
+ TODO: Missing Lead Funding
+ TODO: Missing Status of agents
+ TODO: Missing registration date
+*/
+
 export const GET_ALL_AGENTS_QUERY = gql`
   query GetAgents($first: Int, $page: Int) {
     agents(first: $first, page: $page) {
