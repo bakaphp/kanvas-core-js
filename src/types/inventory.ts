9fdfae8eccf45c6f043e05d362ac13d97c899e26
jerlyrosa <jerlymigueldelarosah@gmail.com>
2023-10-16 10:57:23 -0400

    fix(inventory): fix product_status type

--- src/types/inventory.ts
+++ src/types/inventory.ts
@@ -322,6 +322,6 @@ export interface ProductDashboardInterface {
   productDashboard: {
     total_products: number;
     total_variants: number;
-    product_status: [];
+    product_status: any
   };
 }
