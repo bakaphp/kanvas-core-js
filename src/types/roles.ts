09c2f593b83321b610fb4a02b3409737158475e7
jerlyrosa <jerlymigueldelarosah@gmail.com>
2023-11-02 17:56:27 -0400

    refactor: Rename Roles enum to RolesEnum and add new interfaces

--- src/types/roles.ts
+++ src/types/roles.ts
@@ -1,8 +1,21 @@
-export enum Roles {
+export enum RolesEnum {
   OWNER = 'Owner',
   ADMIN = 'Admin',
   USER = 'Users',
   AGENT = 'Agents',
   DEVELOPER = 'Developer',
   MANAGER = 'Managers',
+}
+
+export interface RolesInterface {
+  id: string | number;
+  name: string;
+  title: string;
+  scope: string;
+}
+
+export interface CreatedRoles {
+  roles: {
+    data: RolesInterface;
+  };
 }
