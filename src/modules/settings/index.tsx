795c06ec51272fac94643ffa7b92230e9154c62d
RivierGrullon <grullonrivier@gmail.com>
2024-01-05 21:25:30 -0400

    fix: Await mutation in setUserSettings method

--- src/modules/settings/index.tsx
+++ src/modules/settings/index.tsx
@@ -64,7 +64,7 @@ export default class Settings {
 
   async setUserSettings(input: ConfigInput): Promise<boolean | undefined> {
     try {
-      const data = this.client.mutate({
+      await this.client.mutate({
         mutation: SET_USER_SETTINGS_MUTATION,
         variables: {
           input,
