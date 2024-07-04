5595acee85b0661edd5a96f5c2c6ebbc6ae29c14
kaioken <max@mctekk.com>
2024-07-04 16:13:17 -0400

    feat: add user social property

--- src/queries/users.query.ts
+++ src/queries/users.query.ts
@@ -42,6 +42,51 @@ export const GET_USER_DATA_QUERY = gql`
   }
 `;
 
+export const GET_USER_SOCIAL_DATA_QUERY = gql`
+  query {
+    me {
+      id
+      uuid
+      firstname
+      lastname
+      displayname
+      default_company
+      default_company_branch
+      email
+      welcome
+      mainRole
+      social {
+        total_message
+      }
+      branches {
+        id
+        name
+        phone
+      }
+      companies {
+        id
+        name
+        uuid
+      }
+      contact {
+        phone_number
+        cell_phone_number
+      }
+      roles
+      abilities
+      custom_fields(orderBy: [{ column: UPDATED_AT, order: DESC }]) {
+        data {
+          name
+          value
+        }
+      }
+      photo {
+        url
+      }
+    }
+  }
+`;
+
 export const GET_ROLE_ID_BY_NAME_QUERY = gql`
   query GetRolesByName($where: QueryRolesWhereWhereConditions!) {
     roles(where: $where) {
