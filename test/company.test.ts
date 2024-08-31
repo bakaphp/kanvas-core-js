8442675e81ec6a31aff0ca8c87c50297ae8bfe80
kaioken <max@mctekk.com>
2024-08-30 23:03:49 -0400

    feat: upload company avatar

--- test/company.test.ts
+++ test/company.test.ts
@@ -7,6 +7,9 @@ beforeAll(async () => {
     await initializeClient();
 });
 
+/**
+ * @todo add profile photo upload test
+ */
 describe('Test the Kanvas Company client', () => {
 
     it('gets user companies', async () => {
@@ -16,22 +19,5 @@ describe('Test the Kanvas Company client', () => {
         const userCompanies = await client.companies.getCompanies();
         expect(userCompanies).toBeDefined();
         expect(userCompanies.companies.data[0].name).toBeDefined();
-    });
-
-    it('uploads company profile photo', async () => {
-        /*         const client = getClient();
-        
-                const userCompanies = await client.companies.getCompanies();
-         */
-        //const mockFile = createMockFile('dummy content', 'test.png', 'image/png');
-
-        //const companyId = userCompanies.companies.data[0].id;
-        //const response = await client.filesystem.updateCompanyPhotoProfile(mockFile, companyId);
-        /*         expect(response).toBeDefined();
-                expect(response.id).toBe(companyId); */
-        /**
-         * @todo help fix my error 
-         * TypeError: source.on is not a function
-         */
     });
 });
