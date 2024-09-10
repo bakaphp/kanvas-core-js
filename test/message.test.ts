2399954c9364a29158bfe2265125b0479d923ba7
Rafael White <rwhite@mctekk.com>
2024-09-10 15:27:48 -0400

    add attachFileToMessage test

--- test/message.test.ts
+++ test/message.test.ts
@@ -233,4 +233,27 @@ describe('Test the Social Messages', () => {
         const deleted = await messages.deleteAllMessages();
         expect(deleted).toBe(true);
     });
+
+    it('attach file to message', async () => {
+      const client = getClient();
+      const messages = client.messages;
+      const messageContent = 'Hello, Kanvas!';
+      const newMessage = await messages.createMessage({
+        message_verb: 'post',
+        message: messageContent,
+      });
+
+      expect(newMessage).toBeDefined();
+      expect(newMessage.id).toBeDefined();
+      expect(newMessage.message).toBe(messageContent);
+
+      const file = new File(['This is some content'], 'file.txt', {
+        type: 'text/plain',
+      });
+
+      const attachFileToMessage = await messages.attachFileToMessage(newMessage.id, file);
+
+      console.log(attachFileToMessage);
+      expect(attachFileToMessage).toBeDefined();
+    });
 });
